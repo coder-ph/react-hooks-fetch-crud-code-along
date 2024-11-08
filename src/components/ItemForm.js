6234d606263f13c3962ee03acad7b036b897fe55
Phelix <phelixmbani@gmail.com>
2024-11-08 14:55:56 +0300

    fetch-crud

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
+import { Headers } from "whatwg-fetch";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((response) => response.json())
+      .then((newItem) => onAddItem(newItem));
+  }
 
   return (
     <form className="NewItem">
@@ -29,7 +49,9 @@ function ItemForm() {
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button onClick={handleSubmit} type="submit">
+        Add to List
+      </button>
     </form>
   );
 }
